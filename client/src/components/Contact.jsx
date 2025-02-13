a784c7fd6e00f7dadcb2a8bf11e093e4c1baf66a
Abdul Rahman <Abdulrahman.81869@gmail.com>
2025-02-13 07:20:45 +0000

    React Portfolio

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <>
-     
-     <div className="border-b border-neutral-900 pb-20 ">
-        <h2 className="my-10 text-center text-4xl">Get in Touch </h2>
-        <div className="text-center tracking-tight">
-            <p className='my-4'>address: Masab Tank, Hyderabad, TS 10153 "</p>
-            <p className=''>phoneNo: +91 8186978069</p>
-            <p className='border-b w-60 text-center ml-108 mt-5'>email: abdulrahman.81869@gmail.com</p>
-        </div>
-     </div>
-    
-    </>
-  )
-}
+    <div className="border-b border-neutral-900 pb-20 px-4">
+      <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+      <div className="text-center tracking-tight max-w-lg mx-auto">
+        <p className="my-4">Address: Masab Tank, Hyderabad, TS 10153</p>
+        <p>Phone No: +91 8186978069</p>
+        <p className="border-b w-60 mx-auto mt-5">
+          Email: abdulrahman.81869@gmail.com
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
